f3f89845b3106dbf5171cfb783508dcf6748faf5
shravya-chepa <riya.chepa@gmail.com>
2025-09-20 09:52:30 -0500

    Initial Commit

---
+++ ReaderAssistant/ReaderAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderAssistantApp.swift
+//  ReaderAssistant
+//
+//  Created by Shravya Chepa on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ReaderAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
